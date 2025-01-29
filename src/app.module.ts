fde82f75237234d5bad7edaa06fe40ba1b72a46c
Stephen <s.klop@xitra.nl>
2025-01-29 12:31:04 +0100

    style(app): Added initial data modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './modules/order/order.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { ShippingModule } from './modules/shipping/shipping.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, PaymentModule, InventoryModule, ShippingModule],
   controllers: [AppController],
   providers: [AppService],
 })
